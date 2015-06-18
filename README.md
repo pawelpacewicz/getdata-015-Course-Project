5355830c0c8885396e7cec2c0e6f7a3d4d0e395d
Pawe³ Pacewicz <pawelpacewicz@gmail.com>
2015-06-18 16:13:55 +0200

    3rd commit

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # README for Course Project on Getting and Cleaning Data (getdata-015)
 
 
+
 ***
 ##Table of Content:
 
--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 library(dplyr)
 
-
 #REQ 1: Merges the training and the test sets to create one data set.
 #====================================================================
 
